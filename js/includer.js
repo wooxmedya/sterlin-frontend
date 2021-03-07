d4a41bf634e6766bebe50726a673d9edc5362ee9
orhancekic <cekicbusiness@gmail.com>
2021-03-07 17:44:34 +0300

    Madencilik hesaplama sayfası yapıldı

--- js/includer.js
+++ js/includer.js
@@ -1,14 +1,11 @@
 //This file handle basic client-side including in Html
 function includeHTML() {
     var z, i, elmnt, file, xhttp;
-    /* Loop through a collection of all HTML elements: */
     z = document.getElementsByTagName("include");
     for (i = 0; i < z.length; i++) {
         elmnt = z[i];
-        /*search for elements with a certain atrribute:*/
         file = elmnt.getAttribute("include-html");
         if (file) {
-            /* Make an HTTP request using the attribute value as the file name: */
             xhttp = new XMLHttpRequest();
             xhttp.onreadystatechange = function() {
                 if (this.readyState == 4) {
@@ -18,14 +15,12 @@ function includeHTML() {
                     if (this.status == 404) {
                         elmnt.innerHTML = "Page not found.";
                     }
-                    /* Remove the attribute, and call this function once more: */
                     elmnt.removeAttribute("include-html");
                     includeHTML();
                 }
             }
             xhttp.open("GET", file, true);
             xhttp.send();
-            /* Exit the function: */
             return;
         }
     }
